e8a9da5c82d9fbcb30f28839a6f91e0cb9c46d57
breynerciro <bciro@unal.edu.co>
2025-02-19 09:23:52 -0500

    apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -121,7 +121,6 @@ const App = () => {
                 
                 <Layout>
                   <MyBookings />
-                  <EditUser />
                 </Layout>
               }
             />
