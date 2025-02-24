f2557964f9ae0e1e95fd2aa23925ce0501f6f930
Daniel Gonzalez <dgonzalezca@unal.edu.co>
2025-02-24 16:04:53 -0500

    limpieza de código

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,7 @@
-import PrivacyPolicy from "./PrivacyPolicy";
-import TermsOfService from "./TermsOfService";
-import Modal from "react-modal";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [modalContent, setModalContent] = useState<React.ReactNode>(undefined);
 
-  const openModal = (content: React.ReactNode) => {
-    setModalContent(content);
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
 
   return (
     <div className="bg-rose-500 py-10 px-4 sm:px-6 lg:px-8 max-w-screen">
@@ -27,40 +13,6 @@ const Footer = () => {
             <img src="/logo.svg" alt="Logo" className="w-8 h-8 mr-2" />
             Workio
           </span>
-
-	  {/* 
-	<div>
-		<button
-                onClick={() => openModal(<PrivacyPolicy />)}
-                className="text-sm md:text-base hover:text-rose-200 transition-colors text-white font-bold"
-              >
-                Política de Privacidad
-              </button>
-            </Link>
-
-            <Link to="/TermsOfService">
-              <button
-                onClick={() => openModal(<TermsOfService />)}
-                className="text-sm md:text-base hover:text-rose-200 transition-colors text-white font-bold"
-              >
-                Términos de Nuestro Servicio
-              </button>
-
-	<div className="flex flex-col md:flex-row gap-4 md:gap-6 text-center md:text-left">
-
-            <Link to='/PrivacyPolicy'>
-              <button
-                onClick={() => openModal(<PrivacyPolicy />)}
-                className="text-sm md:text-base hover:text-rose-200 transition-colors text-white font-bold"
-              >
-                Política de Privacidad
-              </button>
-            </Link>
-
-            <Link to="/TermsOfService">
-
-
-	 */}
 
           {/* Enlaces centrados en móviles, alineados a la derecha en pantallas grandes */}
           <div className="flex flex-col md:flex-row gap-4 md:gap-6 text-center md:text-left">
@@ -90,51 +42,6 @@ const Footer = () => {
           </p>
         </div>
       </div>
-
-      {/* Modal */}
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Información adicional"
-        style={{
-          overlay: {
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-          },
-          content: {
-            width: "90%",
-            height: "70vh",
-            maxHeight: "500px",
-            margin: "auto",
-            borderRadius: "8px",
-            padding: "20px",
-            maxWidth: "600px",
-          },
-        }}
-        closeTimeoutMS={0}
-      >
-        <div className="flex justify-end">
-          <button
-            onClick={closeModal}
-            className="text-gray-400 hover:text-gray-600"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-        {modalContent && modalContent}
-      </Modal>
     </div>
   );
 };
