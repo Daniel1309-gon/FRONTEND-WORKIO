b8f72c968579609131d9538d5ad868e15e335f04
jaim3s <jujaimesc@unal.edu.co>
2025-02-24 00:30:24 -0500

    Fade out y reposicionamiento para los mensajes de exito y error

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type ToastProps = {
   message: string;
@@ -7,9 +7,17 @@ type ToastProps = {
 };
 
 const Toast = ({ message, type, onClose }: ToastProps) => {
+  const [visible, setVisible] = useState(false);
+
   useEffect(() => {
+    // Mostrar el toast con efecto de fade-in
+    setVisible(true);
+
     const timer = setTimeout(() => {
-      onClose();
+      // Iniciar fade-out
+      setVisible(false);
+      // Llamar a onClose cuando finalice la transición (500ms)
+      setTimeout(onClose, 1000);
     }, 5000);
 
     return () => {
@@ -17,10 +25,12 @@ const Toast = ({ message, type, onClose }: ToastProps) => {
     };
   }, [onClose]);
 
-  const styles =
-    type === "SUCCESS"
-      ? "fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
-      : "fixed top-4 right-4 z-50 p-4 rounded-md bg-red-600 text-white max-w-md";
+  const styles = `${
+    type === "SUCCESS" ? "bg-green-600" : "bg-red-600"
+  } fixed top-20 right-4 z-50 p-4 rounded-md text-white max-w-md 
+     transition-opacity duration-500 ${
+       visible ? "opacity-100" : "opacity-0"
+     }`;
 
   return (
     <div className={styles}>
