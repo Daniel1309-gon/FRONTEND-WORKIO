cab58e0dd5790ef2d28fbed28d708b5e616a73d0
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2025-02-23 23:21:31 -0500

    Responsive Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,6 +22,28 @@ const Header = () => {
           {/* Oculta "Workio" en pantallas pequeñas */}
         </Link>
 
+	{/* 
+	<span className="flex space-x-2 sm:space-x-3">
+          {isLoggedIn ? (
+            <>
+              {user?.role === "user" && (
+                <Link
+                  className="flex items-center text-white px-3 py-1 sm:px-4 sm:py-2 text-sm sm:text-base font-bold hover:bg-blue-600 rounded"
+                  to="/bookings"
+                >
+                  Mis Reservas
+                </Link>
+              )}
+              {user?.role === "admin" && (
+                <Link
+                  className="flex items-center text-white px-3 py-1 sm:px-4 sm:py-2 text-sm sm:text-base font-bold hover:bg-blue-600 rounded"
+                  to="/my-coworkings"
+                >
+                  Mis Coworkings
+                </Link>
+              )}
+	*/}
+
         {/* Menú de navegación */}
         <span className="flex space-x-2 sm:space-x-3">
           {isLoggedIn ? (
